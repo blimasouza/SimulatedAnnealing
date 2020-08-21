23f9b283ff171b6401bd0e237c22ffbf38816a84
Lima Bruno <lima.bruno@bcg.com>
2020-08-21 20:03:57 -0300

    adjust the test file to produce a simple example with a concrete implementation of a state, move! and energy functions

--- src/test.jl
+++ src/test.jl
@@ -1,17 +1,43 @@
 module test
 
-import SimulatedAnnealing
+import SimulatedAnnealing: AbstractState, move!, energy, Parameters, optimize
+import Distributions: Normal
 import Printf: @printf
 
-state = SimulatedAnnealing.State(100, 1)
-params = SimulatedAnnealing.Parameters(1000, 20, 100, 1)
+# define a concrete state
+mutable struct State <: AbstractState
+    value::Number
+    σ::Number
+    State(value::Number) = new(value, 0.01)
+    State(value::Number, σ::Number) = new(value, σ)
+end
 
-result = SimulatedAnnealing.optimize(state, params)
+# define how we move
+function move!(state::State)
+    dist = Normal(0, state.σ)
+    ϵ = rand(dist)
+    state.value += ϵ
+end
+
+# define how we compute the energy
+function energy(state::State)
+    x = state.value
+    return x^2 - 5x + 6
+end
+
+# setup the initial state
+state = State(100, 1)
+
+# setup the optimization params
+params = Parameters(1000, 20, 100, 1)
+
+# optimize
+result = optimize(state, params)
 
 @printf(
     "Best state: %.3f | Best energy: %.3f \n", 
     result.value,
-    SimulatedAnnealing.energy(result)
+    energy(result)
 )
 
 end
