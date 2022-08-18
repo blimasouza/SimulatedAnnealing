59740cdc62290cdd33e7536af0db1e220012c6dd
Bruno Lima <lima.bruno@bcg.com>
2022-08-17 21:58:25 -0300

    refactor the optimization procedure.
    * simplify the update function
    * keep track of the optimization history

--- src/methods/optimize.jl
+++ src/methods/optimize.jl
@@ -8,31 +8,57 @@ function accept_move(ΔE, temperature)
     end
 end
 
-
 function update!(
     state::AbstractState,
-    previous_state::AbstractState,
-    previous_energy::Number,
-    best_state::AbstractState,
+    original_energy::Number,
+    best_energy::Number,
     current_temp::Number,
+    history::Vector{Iteration},
+    step::Integer,
 )
+    # keep track of the original state
+    original_state = deepcopy(state)
+
+    # make a moviment     
     move!(state)
-    current_energy = energy(state)
-    ΔE = current_energy - previous_energy
+
+    # compute the new energy and the energy gap     
+    new_energy = energy(state)
+    ΔE = new_energy - original_energy
+
+    energy_improve = false
 
     if accept_move(ΔE, current_temp)
-        previous_state = deepcopy(state)
-        previous_energy = current_energy
-        if ΔE < 0 && current_energy < energy(best_state)
+        if ΔE < 0 && new_energy < best_energy
             best_state = deepcopy(state)
+            best_energy = energy(best_state)
+            energy_improve = true
         end
+        update_history!(
+            history,
+            step,
+            state,
+            new_energy,
+            best_energy,
+            current_temp,
+            true,
+            energy_improve,
+        )
+        return state, new_energy, best_state, best_energy
     else
-        state = deepcopy(previous_state)
+        update_history!(
+            history,
+            step,
+            original_state,
+            original_energy,
+            best_energy,
+            current_temp,
+            false,
+            energy_improve,
+        )
+        return original_state, original_energy, best_state, best_energy
     end
-
-    return state, previous_state, previous_energy, best_state
 end
-
 
 function optimize(state::AbstractState, params::Parameters)
     # define the cooling_schedule
@@ -40,26 +66,45 @@ function optimize(state::AbstractState, params::Parameters)
 
     # initialize the best state
     best_state = deepcopy(state)
+    best_energy = energy(best_state)
 
     # initialize the current temperature
     current_temp = params.maxtemp
 
-    # keep track of the previous state
-    previous_state = deepcopy(state)
-
     # initialize the energy
-    previous_energy = energy(state)
+    current_energy = energy(state)
 
-    for step = 1:params.steps
-        state, previous_state, previous_energy, best_state = update!(
-            state, previous_state, previous_energy, best_state, current_temp
+    # initialize history array
+    history = initialize_history(
+        params,
+        state,
+        current_energy,
+        best_energy,
+        current_temp,
+    )
+
+    for step in 1:params.steps
+        state, current_energy, best_state, best_energy = update!(
+            state,
+            current_energy,
+            best_energy,
+            current_temp,
+            history,
+            step,
         )
-        if step % Int(floor(params.steps / params.updates)) == 1 || step == params.steps
-            report(step, params.steps, current_temp, previous_energy)
+
+        if step % Int(floor(params.steps / params.updates)) == 1 ||
+           step == params.steps
+            report(
+                step,
+                params.steps,
+                current_temp,
+                current_energy,
+                best_energy,
+            )
         end
         current_temp = temperature(step)
     end
 
-    return best_state
+    return best_state, history
 end
-
