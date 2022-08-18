eb162ca3da5294bc6fc446ad0ea3952da321973d
Bruno Lima <lima.bruno@bcg.com>
2022-08-17 21:49:26 -0300

    adjust the reporting function to also display the best energy so far

--- src/utils/report.jl
+++ src/utils/report.jl
@@ -1,24 +1,28 @@
 import Printf: @printf
 
-
-function report(step, max_steps, temperature, energy)
+function report(step, max_steps, temperature, energy, best_energy)
     separator = (
-            "| " * repeat("-", 10)
-        * " | " * repeat("-", 11)
-        * " | " * repeat("-", 10)
-        * " |"
+        "| " *
+        repeat("-", 10) *
+        " | " *
+        repeat("-", 11) *
+        " | " *
+        repeat("-", 10) *
+        " | " *
+        repeat("-", 11) *
+        " |"
     )
     if step == 1
         println(separator)
         @printf(
-            "| %10s | %11s | %10s |\n",  
-            ["step", "temperature", "energy"]...
+            "| %10s | %11s | %10s | %11s |\n",
+            ["step", "temperature", "energy", "best energy"]...
         )
         println(separator)
     end
     @printf(
-        "| %10d | %11.0f | %10.2f |\n", 
-        [step, temperature, energy]...
+        "| %10d | %11.0f | %10.2f | %11.2f |\n",
+        [step, temperature, energy, best_energy]...
     )
     if step == max_steps
         println(separator)
